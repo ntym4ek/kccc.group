6975aeb9da9543e5fbfc7059ac67923171cde1eb
ntym4ek <mega_comp@mail.ru>
2023-10-16 11:36:31 +0300

    правка отображения слайдеров на телефонах

--- sites/all/modules/custom/ext_block/templates/block-divisions.tpl.php
+++ sites/all/modules/custom/ext_block/templates/block-divisions.tpl.php
@@ -15,7 +15,7 @@
       <div class="row">
         <div class="col-xs-12">
 
-          <div id="carousel-divisions" class="carousel carousel-divisions outer-pagination outer-navigation" data-slidesperview="3">
+          <div id="carousel-divisions" class="carousel carousel-divisions outer-pagination outer-navigation" data-slidesperview-xs="1" data-slidesperview-md="2" data-slidesperview-lg="3">
             <div class="swiper">
               <div class="swiper-wrapper">
                 <?php foreach ($cards as $card) {
--- sites/all/modules/custom/ext_block/templates/block-news.tpl.php
+++ sites/all/modules/custom/ext_block/templates/block-news.tpl.php
@@ -13,7 +13,7 @@
 
   <div class="row">
     <div class="col-xs-12">
-      <div id="carousel-news" class="carousel carousel-news outer-pagination" data-slidesperview="2">
+      <div id="carousel-news" class="carousel carousel-news outer-pagination" data-slidesperview-xs="1" data-slidesperview-md="2" data-slidesperview-lg="3">
         <div class="swiper">
           <div class="swiper-wrapper">
             <?php foreach ($cards as $card): ?>
