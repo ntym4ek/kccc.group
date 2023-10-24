e13492571b9b13ab7b7d18d71de12b6a319d02ac
ntym4ek <mega_comp@mail.ru>
2023-10-24 16:04:39 +0300

    карточки подразделений на странице О компании сделаны активными

--- sites/all/modules/custom/ext_block/templates/block-structure.tpl.php
+++ sites/all/modules/custom/ext_block/templates/block-structure.tpl.php
@@ -19,30 +19,38 @@
       </div>
     </div>
     <div class="col-xs-12 col-md-4">
-      <div class="division">
-        <div class="logo"><img src="/sites/all/modules/custom/ext_block/images/logo-td.png" alt=""></div>
-        <div class="name"><?php print t("TH “Kirovo-Chepetsk Chemical Company” LLC"); ?></div>
-      </div>
+      <a href="https://kccc.ru"  target="_blank">
+        <div class="division hover-raise">
+          <div class="logo"><img src="/sites/all/modules/custom/ext_block/images/logo-td.png" alt=""></div>
+          <div class="name"><?php print t("TH “Kirovo-Chepetsk Chemical Company” LLC"); ?></div>
+        </div>
+      </a>
     </div>
   </div>
   <div class="row center-xs">
     <div class="col-xs-12 col-md-4">
-      <div class="division">
-        <div class="logo"><img src="/sites/all/modules/custom/ext_block/images/logo-strada.png" alt=""></div>
-        <div class="name"><?php print t("“Strada” LLC"); ?></div>
-      </div>
+      <a href="https://joy-magazin.ru" target="_blank">
+        <div class="division hover-raise">
+          <div class="logo"><img src="/sites/all/modules/custom/ext_block/images/logo-strada.png" alt=""></div>
+          <div class="name"><?php print t("“Strada” LLC"); ?></div>
+        </div>
+      </a>
     </div>
     <div class="col-xs-12 col-md-4">
-      <div class="division">
-        <div class="logo"><img src="/sites/all/modules/custom/ext_block/images/logo-eva.png" alt=""></div>
-        <div class="name"><?php print t("“Eva” LLC"); ?></div>
-      </div>
+      <a href="https://eva-si.ru" target="_blank">
+        <div class="division hover-raise">
+          <div class="logo"><img src="/sites/all/modules/custom/ext_block/images/logo-eva.png" alt=""></div>
+          <div class="name"><?php print t("“Eva” LLC"); ?></div>
+        </div>
+      </a>
     </div>
     <div class="col-xs-12 col-md-4">
-      <div class="division">
-        <div class="logo"><img src="/sites/all/modules/custom/ext_block/images/logo-smola.png" alt=""></div>
-        <div class="name"><?php print t("“Smola” LLC"); ?></div>
-      </div>
+      <a href="https://smola.kccc.ru" target="_blank">
+        <div class="division hover-raise">
+          <div class="logo"><img src="/sites/all/modules/custom/ext_block/images/logo-smola.png" alt=""></div>
+          <div class="name"><?php print t("“Smola” LLC"); ?></div>
+        </div>
+      </a>
     </div>
   </div>
 
